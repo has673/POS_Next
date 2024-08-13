e6f1ad8157f2e34a93d05c09f3f65c5aea9aff58
has673 <hassansajjas>
2024-08-13 17:20:49 +0500

    recover page

---
+++ src/app/Password/page.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Heading from "../Components/Heading";
+
+const page = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center rounded-4xl">
+      <Heading />
+      <div className="bg-bg h-card w-card1 text-center  ">
+        <h1 className="text-white">Recover</h1>
+      </div>
+    </div>
+  );
+};
+
+export default page;
