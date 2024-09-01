e15a803e1dece57423f6a6d19613f362c0ed6bb4
has673 <hassansajjas>
2024-09-01 20:33:53 +0500

    loading;

--- src/app/(Dashboard)/Order/page.js
+++ src/app/(Dashboard)/Order/page.js
@@ -4,6 +4,7 @@ import Heading from "@/Components/Heading";
 import OrderCard from "@/Components/orderCard";
 
 import Subheading from "@/Components/Subheading";
+import { Spin } from "antd";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 
@@ -12,16 +13,30 @@ import React, { useEffect, useState } from "react";
 const page = () => {
   const router = useRouter();
   const [orders, setOrders] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6;
+
+  const totalPages = Math.ceil(orders.length / itemsPerPage);
+  // console.log(items.length);
+  // Slice the data to display only the items for the current page
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentData = orders.slice(indexOfFirstItem, indexOfLastItem);
+
   const handleClick = () => {
     router.push("/CreateOrder");
   };
   const getData = async () => {
     try {
+      setLoading(true);
       const response = await axios.get("http://localhost:4000/order");
       console.log(response);
       setOrders(response.data);
     } catch (err) {
       console.log(err);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -29,6 +44,9 @@ const page = () => {
     getData();
   }, []);
 
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
   return (
     <div className="bg-black w-full text-white">
       <div className="flex justify-between">
@@ -41,11 +59,32 @@ const page = () => {
           Add New Order
         </button>
       </div>
-      <div className="grid grid-cols-3 gap-2">
-        {orders.map((order) => (
-          <OrderCard key={order.id} order={order} />
-        ))}
-      </div>
+      <Spin spinning={loading}>
+        <div className="grid grid-cols-3 gap-2">
+          {currentData.map((order) => (
+            <OrderCard key={order.id} order={order} />
+          ))}
+        </div>
+        <div className="flex justify-center gap-3 mt-5">
+          <button
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+            className="bg-gray-600 text-white px-4 py-2 rounded-md mr-2"
+          >
+            Previous
+          </button>
+          <span className="text-white mt-2">
+            Page {currentPage} of {totalPages}
+          </span>
+          <button
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+            className="bg-gray-600 text-white px-4 py-2 rounded-md ml-2"
+          >
+            Next
+          </button>
+        </div>
+      </Spin>
     </div>
   );
 };
