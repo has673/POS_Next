31826fc7cf55c8cb628bc9e142ca1107b9ba7cdd
has673 <hassansajjas>
2024-09-01 19:39:43 +0500

    chNGES

--- src/Components/orderCard.js
+++ src/Components/orderCard.js
@@ -7,7 +7,7 @@ const OrderCard = ({ order }) => {
     <div className="bg-bg w-22vw rounded-sm ml-3 h-auto">
       <div className="flex justify-between gap-2">
         <div className="flex justify-center gap-2">
-          <h2 className="bg-pink rounded-sm text-black w-auto p-1 m-1 h-auto">
+          <h2 className="bg-pink rounded-sm text-black w-auto p-1.5 m-1 h-2/3 ml-2 ">
             {order.id}
           </h2>
           <div>
@@ -58,7 +58,32 @@ const OrderCard = ({ order }) => {
           <div className="text-white mr-3  ">price</div>
         </div>
       </div>
+      {order.orderItems && order.orderItems.length > 0 ? (
+        order.orderItems.map((item, index) => (
+          <div
+            key={index}
+            className="flex justify-between gap-2 text-white text-xs px-2 py-1"
+          >
+            <div className="flex justify-start gap-2">
+              <h2 className="ml-2">{item.quantity}</h2>
+              <p>{item.name}</p>
+            </div>
+            <div>
+              <div className="text-white mr-3">{item.price}</div>
+            </div>
+          </div>
+        ))
+      ) : (
+        <div className="text-center text-xs text-white py-2">
+          No items in order
+        </div>
+      )}
+
       <hr></hr>
+      <div className="flex justify-between gap-3 text-xs mt-3">
+        <h4 className="mx-3"> Sub Total</h4>
+        <h4 className="mx-3">{order.totalPrice}</h4>
+      </div>
       <div className="flex justify-center gap-2 my-3">
         <div className="w-1/5 border-2 border-pink p-2  flex justify-center items-center rounded-md ">
           <LuPen className="text-pink " />
